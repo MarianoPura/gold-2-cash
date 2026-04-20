2fc045840ffa229240f3041c5c6ca40ad99f0503
ianno <marianopuraiii@gmail.com>
2026-04-20 09:48:50 +0800

    initial commit

---
+++ arduino/arduino.ino
@@ -0,0 +1,44 @@
+#include <HX711_ADC.h>
+
+#define DT 3
+#define SCK 2
+#define BUTTON 7
+#define LED 8
+#define LED 
+
+HX711 scale;
+
+float calibration_factor = -7050;
+
+void setup() {
+  Serial.begin(9600);
+  scale.begin(DT, SCK);
+  scale.set_scale(calibration_factor);
+  scale.tare();
+
+  pinMode(BUTTON, INPUT_PULLUP);
+
+}
+
+void loop() {
+  float weight = scale.get_units(5);
+
+  Serial.print('WEIGHT:');
+  Serial.println(weight);
+  delay(500);
+
+  if (digitalRead(BUTTON) == LOW) {
+    Serial.println("WINNER");
+
+    for (int i=0; i<6; i++){
+      digitalWrite(LED, HIGH);
+      delay(150)
+      digitalWrite(LED, LOW);
+      delay(150)
+    }
+
+    delay(300);
+  }
+}
+
+delay(200);
---
+++ myenv/podium.py
@@ -0,0 +1,29 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+WEB_PATH = "/opt/lampp/htdocs/arduino-test/"
+
+current_weight = "0"
+
+def show_weight (weight):
+    with open(WEB_PATH + "../weight.txt", "w") as f:
+        f.write(str(weight))
+
+def show_winner ():
+    with open(WEB_PATH + "../stats.txt", "w") as f:
+        f.write(str(1))
+
+while True:
+    line = ser.readline().decode('utf-8').rstrip()
+
+    if line.startswith("WEIGHT:"):
+        weight = line.split(":")[1]
+        current_weight = weight
+        show_weight(weight)
+
+    elif line.startswith("WINNER:"):
+        winner = line.split(":")[1]
+        show_winner(winner)
+    time.sleep(0.1)
