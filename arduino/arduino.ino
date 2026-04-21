45651bbdd7b673c29aec34f3ee95d99319398323
Patrick Lambino <lambino.patrick.competente@gmail.com>
2026-04-21 17:18:00 +0800

    arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,34 +3,70 @@
 #define DT 13
 #define SCK 16
 #define BUTTON 17
+#define TARE 25 
+#define BUTTONWEIGHT 26
 
 HX711 scale;
 
-float calibration_factor = 108.64;
+float calibration_factor = 108.55;
 //108.55 iba pang stable calibration
+int lastTareState = HIGH;
+int lastResetState = HIGH;
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   scale.begin(DT, SCK);
   scale.set_scale(calibration_factor);
   scale.tare();
 
   pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(TARE, INPUT_PULLUP);
+  pinMode(BUTTONWEIGHT, INPUT_PULLUP);
+
 
 }
 
+void TARE_FUNC() {
+  int currentState = digitalRead(TARE);
+
+  if (lastTareState == HIGH && currentState == LOW) {
+    scale.tare();
+    Serial.println("TARE DONE");
+  }
+
+  lastTareState = currentState;
+}
+
+// void RESET_FUNC(){
+
+//   int currentState = digitalRead(RESET);
+
+//   if(lastResetState == HIGH && currentState == LOW){
+//     Serial.println("RESET DONE");
+//   }
+
+//   lastResetState = currentState;
+
+// }
+
 void loop() {
+  TARE_FUNC();
   float weight = scale.get_units(10);
 
   if (digitalRead(BUTTON) == LOW) {
     Serial.println("WINNER");
-    delay(500);
+    delay(50);
   }
 
-  delay(500);
-  Serial.print("WEIGHT:");
-  Serial.println(weight);
+  if (digitalRead(BUTTONWEIGHT) == LOW){
+    Serial.print("WEIGHT:");
+    Serial.println(weight);
+  }
+
+  // Serial.print("WEIGHT:");
+  // Serial.println(weight);
   
-  delay(200);
+  delay(50);
 
 }
